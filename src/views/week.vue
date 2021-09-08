b0e8b6b02cdadbec78d10e06ddf994d5f32f2ee2
mf <13601392033@163.com>
2021-09-08 17:53:29 +0800

    d\

--- src/views/week.vue
+++ src/views/week.vue
@@ -4,56 +4,64 @@
 
         <div class="camera">
             <ul class="stage">
-                <li  class="box" v-for="(item, index) in list" :key="index" :style="{transform: 'rotateY('+(index*45)+'deg) translateZ(310px)'}">
-                    <p class="title">{{$moment(item[0]).format('dddd')}}</p>
-                    <div class="module">
-                        <div class="item-module" v-for="(sonItem,i) in item[1]" :key="i" :class="{'habit-container':sonItem.type==3}">
+                <li  v-for="(item, index) in list" :key="index" >
+                    <div class="box" :style="{transform: 'rotateY('+(index*45)+'deg) translateZ(310px)'}">
+                        <div class="scroll">
+                            <i class="el-icon-top top" @click="boxTop($event, index)"></i>
+                            <i class="el-icon-bottom bottom" @click="boxBottom($event,index)"></i> 
+                        </div>
+                        
+                        <p class="title">{{$moment(item[0]).format('dddd')}}</p>
+                        <div class="module">
+                            <div class="item-module" v-for="(sonItem,i) in item[1]" :key="i" :class="{'habit-container':sonItem.type==3}">
 
-                            <div class="task-module" >
-                                <div v-if="sonItem.type == 1" :class="stateClass(sonItem)">
-                                    <div class="task-title">
-                                        {{sonItem.title}}
-                                    </div>
-                                    <div class="task-content">
-                                        <span>{{sonItem.content}}</span>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="habit-module">
-                                <div v-if="sonItem.type == 3">
-                                    <div class="habit" :style="{'background': sonItem.habit[0].backColor, 'color': sonItem.habit[0].logoColor}">
-                                        <div class="habit-item">
-                                            <i v-if="sonItem.habit[0].logoType == 1" class="icon" :class="iconClass(sonItem.habit[0])"></i>
-                                            <span v-if="sonItem.habit[0].logoType == 0" class="icon">{{sonItem.habit[0].logo}}</span>  
+                                <div class="task-module" >
+                                    <div v-if="sonItem.type == 1" :class="stateClass(sonItem)">
+                                        <div class="task-title">
+                                            {{sonItem.title}}
+                                        </div>
+                                        <div class="task-content">
+                                            <span>{{sonItem.content}}</span>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="record-module" style="clear:both">
-                                <div v-if="sonItem.type == 2" class="narrow">
-                                    <div class="record-title">
-                                        {{sonItem.title}}
-                                    </div>
-                                    <div class="record-content">
-                                        <span>{{sonItem.content}}</span>
+                                <div class="habit-module">
+                                    <div v-if="sonItem.type == 3">
+                                        <div class="habit" :style="{'background': sonItem.habit[0].backColor, 'color': sonItem.habit[0].logoColor}">
+                                            <div class="habit-item">
+                                                <i v-if="sonItem.habit[0].logoType == 1" class="icon" :class="iconClass(sonItem.habit[0])"></i>
+                                                <span v-if="sonItem.habit[0].logoType == 0" class="icon">{{sonItem.habit[0].logo}}</span>  
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="diary-module" style="clear:both">
-                                <div v-if="sonItem.type == 4" class="narrow">
-                                    <div class="record-title">
-                                        {{sonItem.title}}
-                                    </div>
-                                    <div class="record-content">
-                                        <span v-html="sonItem.content"></span>
+                                <div class="record-module" style="clear:both">
+                                    <div v-if="sonItem.type == 2" class="narrow">
+                                        <div class="record-title">
+                                            {{sonItem.title}}
+                                        </div>
+                                        <div class="record-content">
+                                            <span>{{sonItem.content}}</span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>  
+
+                                <div class="diary-module" style="clear:both">
+                                    <div v-if="sonItem.type == 4" class="narrow">
+                                        <div class="record-title">
+                                            {{sonItem.title}}
+                                        </div>
+                                        <div class="record-content">
+                                            <span v-html="sonItem.content"></span>
+                                        </div>
+                                    </div>
+                                </div>  
+                            </div>
                         </div>
                     </div>
+                    
                 </li>
             </ul>
         </div>
@@ -61,6 +69,18 @@
 </template>
 
 <style scoped>
+.top{
+    position: fixed;
+    left:-5px;
+    font-size: 20px;
+}
+.bottom{
+    position: fixed;
+    left:-5px;
+    bottom:0;
+    font-size: 20px;
+    
+}
 .locking{
     overflow: auto!important;;
 }
@@ -139,7 +159,7 @@
 .box{
     position: absolute;
     border:1px solid #ddd;
-    overflow: auto  ;
+    overflow: hidden;
     margin:10px;
     height:70%;
     width:235px;
@@ -184,7 +204,6 @@ import $ from 'jquery'
 import {initWeek} from "@/request/week.js"
 function throttle(fn,delay,ev){
     let valid = true
-    console.log(delay)
     return ()=>{
        if(!valid){
            //休息时间 暂不接客
@@ -198,12 +217,16 @@ function throttle(fn,delay,ev){
         }, delay)
     }
 }
+
+
+
 export default {
     data(){
         return {
             list:[],
             back: require("./../assets/m4.jpg"),
             isNull:false,
+            num:100,
         }
     },
     created(){
@@ -220,18 +243,53 @@ export default {
         this.init()
     },
     methods:{
+        boxTop(e, index){
+            let top;
+            if(e.target.style.top){
+                top = parseInt(e.target.style.top.substring(0,e.target.style.top.length-2));
+            }else{
+                top = 0;
+            }
+            if(top == 0){
+                return false;
+            }
+            e.target.style.top = top - this.num + "px"
+            let box = document.getElementsByClassName("box")[index]
+            let bottom = document.getElementsByClassName("bottom")[index]
+            bottom.style.bottom = this.num - top  + "px"
+            box.scrollTop = top - this.num;
+        },
+        boxBottom(e, index){
+            let bottom;
+            let box = document.getElementsByClassName("box")[index]
+            let top = document.getElementsByClassName('top')[index]
+            if(e.target.style.bottom){
+                bottom = parseInt(e.target.style.bottom.substring(0,e.target.style.bottom.length-2));
+            }else{
+                bottom = 0;
+            }
+            if(Math.abs(bottom) > $(box).children(".module")[0].offsetHeight-100){
+                return false;
+            }
+            e.target.style.bottom = bottom - this.num + "px"
+            
+            top.style.top = this.num - bottom + "px"
+            box.scrollTop = box.scrollTop + this.num;
+        },
         init(){
             $(function () {
                 var roY = 0, roX = -10;
                 var xN = 0, yN = 0;
                 var play = null;
-
+                let log = 0;
                 $(".week").on('touchstart', (e) =>{
                     let x_ = e.targetTouches[0].clientX;
                     let y_ = e.targetTouches[0].clientY;
                     clearInterval(play);
+                    log = 0;
                     $(this).bind('touchmove',(ev) =>{
                         throttle((ev)=>{
+                            log = 1;
                             /*获取当前鼠标的坐标*/
                             var x = ev.targetTouches[0].clientX;
                             var y = ev.targetTouches[0].clientY;
@@ -259,9 +317,12 @@ export default {
                             }
                             roY += xN * 0.2;
                             roX -= yN * 0.1;
-                            $('.stage').css({
+                            if(log != 0){
+                                $('.stage').css({
                                 transform: 'perspective(1500px) rotateX('+roX+'deg) rotateY(' + roY + 'deg)'
                             });
+                            }
+                            
                         }, 30);
                     });
             });
